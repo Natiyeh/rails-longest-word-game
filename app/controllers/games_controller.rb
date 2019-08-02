2bd5a8a804eda9d293d22bbd55429a7a9d8c6317
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-02 18:00:11 -0500

    updated score view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,11 +2,11 @@ class GamesController < ApplicationController
   def new
     # display a new random grid and a form
     @letters = ('a'..'z').to_a.sample(10)
-    @letters
   end
 
   def score
+    # byebug
     # The form will be submitted (with POST) to the score action.
-
   end
 end
+
