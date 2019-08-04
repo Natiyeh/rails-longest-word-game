d3896e2f93ac78f1b67a5ee5e2553a020a47f8e6
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-04 18:24:16 -0500

    updated interpolation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,14 @@ class GamesController < ApplicationController
   end
 
   def score
+    if params[:answer].chars.all? { |char| params[:letters].include? char.downcase }
+      @response = "Good job! #{params[:answer].upcase} can be built out of #{params[:letters].split.join(", ")}."
+    else
+      @response = ["test 2"]
+    end
     # byebug
     # The form will be submitted (with POST) to the score action.
   end
 end
 
+# string.chars.any? { |char| ('a'..'z').include? char.downcase }
