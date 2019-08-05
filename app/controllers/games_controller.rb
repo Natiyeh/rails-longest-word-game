e7cf9dd8dac84d9bc3185bd021ebbced78285534
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-08-05 17:14:17 -0500

    added dictionary

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,16 +7,29 @@ class GamesController < ApplicationController
     @letters = ('a'..'z').to_a.sample(10)
   end
 
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+
+  end
+
   def score
-    if params[:answer] == ''
-      @response = "sorry, but you must enter a word."
-    elsif params[:answer].chars.all? { |char| params[:letters].include? char.downcase }
-      @response = "Good job! #{params[:answer].upcase} can be built out of #{params[:letters].split.join(", ")}."
+    if included?(params[:answer], params[:letters])
+      if english_word?(params[:answer])
+        @response = "Good job! #{params[:answer].upcase} can be built out of #{params[:letters].split.join(", ")}."
+      else
+        @response = "Sorry, but #{params[:answer].upcase} is not an english word."
+      end
     else
-      @response = "Sorry, but #{params[:answer].upcase} can't be built out of #{params[:letters].split.join(", ")}."
+      @response = "Sorry, but one or more letters in #{params[:answer].upcase} is not part of #{params[:letters].split.join(", ")}."
     end
     # byebug
     # The form will be submitted (with POST) to the score action.
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 end
 
